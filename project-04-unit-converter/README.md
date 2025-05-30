aa8a8aaa456c81f17c6b31bbef52d7136be2b5a6
Saurabh Parthe <saurabh.parthe.1@gmail.com>
2025-05-30 12:51:01 +0530

    Add Project 04 - Unit Converter

---
+++ project-04-unit-converter/README.md
@@ -0,0 +1,15 @@
+# Project 04 - Unit Converter
+
+A basic Java program that converts kilometers to miles.
+
+## ?? Formula
+
+\\\
+1 kilometer = 0.621371 miles
+\\\
+
+## ?? CI/CD Pipeline
+
+This project includes:
+- GitHub Actions for build/test
+- Maven compilation on every push
---
+++ project-04-unit-converter/src/main/java/com/example/UnitConverter.java
@@ -0,0 +1,9 @@
+package com.example;
+
+public class UnitConverter {
+    public static void main(String[] args) {
+        double kilometers = 10.0;
+        double miles = kilometers * 0.621371;
+        System.out.println(kilometers + " km is equal to " + miles + " miles.");
+    }
+}
